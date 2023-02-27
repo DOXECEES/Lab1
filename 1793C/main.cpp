df48f392838afb32f86bc5011da3788397a4f3da
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2023-02-27 10:46:21 +0300

    Add tests

--- 1793C/main.cpp
+++ 1793C/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <algorithm>
 
-bool Solve(int arr[], int size)
+bool Tests()
+{
+	int arr1[3] = { 1,2,3 };
+	int arr2[4] = { 2,1,4,3 };
+	int arr3[7] = { 1, 3, 2, 4, 6, 5, 7 };
+	int arr4[6] = { 2, 3, 6, 5, 4, 1 };
+
+	if (!(Solve(arr1, 3).first == -1 && Solve(arr1, 3).second == -1))
+	{
+		return false;
+	}
+	if (!(Solve(arr2, 4).first == 1 && Solve(arr2, 4).second == 4))
+	{
+		return false;
+	}
+	if (!(Solve(arr3, 7).first == 2 && Solve(arr3, 7).second == 6))
+	{
+		return false;
+	}
+	if (!(Solve(arr4, 6).first == -1 && Solve(arr4, 6).second == -1))
+	{
+		return false;
+	}
+
+	return true;
+
+}
+
+
+std::pair<int,int> Solve(int arr[], int size)
 {
 	int beginIndex = 0;
 	int endIndex = size - 1;
@@ -31,12 +61,11 @@ bool Solve(int arr[], int size)
 		}
 		else 
 		{
-			std::cout << beginIndex + 1 << ' ' << endIndex + 1 << std::endl;
-			return true;
+			return std::make_pair<beginIndex + 1, endIndex + 1>;
 		}
 	}
 
-	return false;
+	return std::make_pair(-1,-1);
 	
 }
 
@@ -44,6 +73,17 @@ bool Solve(int arr[], int size)
 
 int main()
 {
+
+	if (Tests())
+	{
+		std::cout << "Tests passed" << std::endl;
+	}
+	else
+	{
+		std::cout << "Tests failed" << std::endl;
+		return -1;
+	}
+
 	int t;
 	std::cin >> t;
 	int n;	
@@ -60,9 +100,15 @@ int main()
 			std::cin >> ptr[i];
 		}
 
-		if (!Solve(ptr, n))
+		auto solution = Solve(ptr, n);
+
+		if (solution.first == -1 && solution.second == -1)
 		{
 			std::cout << -1 << std::endl;
+		}
+		else
+		{
+			std::cout << solution.first << ' ' << solution.second << std::endl;
 		}
 
 
